5e92dc84bf43f7cd04f5157056de5be300e6f7c1
Peng-LUH <ethaniel.unihannover@gmail.com>
2026-05-31 15:17:17 +0200

    fixed linting problem in tests

--- app/src/ml_api/test.py
+++
@@ -1,9 +0,0 @@
-from ml_api.model import ModelService
-
-model_service = ModelService()
-
-
-print(model_service.model.eval())
-# prediction = model_service.predict(features=[1, 2, 3])
-
-# print(prediction)
